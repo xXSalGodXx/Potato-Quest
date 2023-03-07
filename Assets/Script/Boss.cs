ff34383e8d4e2a64640df5b3af0bc329cb5f12bf
Sumjoy2 <5011286mountjoy@gmail.com>
2023-03-07 12:15:17 -0800

    BossSetup

--- Assets/Script/Boss.cs
+++ Assets/Script/Boss.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    int lives = 3;
+
+    public float timeInvincible = 1.5f;
+    float invincibleTimer;
+    bool isInvincible;
+
+    public float timerSmall = 3;
+    public float timerBig = 5;
+
+    public GameObject smallFireBall;
+    public GameObject bigFireBall;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,21 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (lives <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (timerSmall > 0)
+        {
+            timerSmall -= Time.deltaTime;
+        }
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 }
